3a30485b747316e92d1c947f6b1a493118dedc3e
carloslorap <74977007@certus.edu.pe>
2022-12-05 22:32:46 -0500

    modified:   anuncios.html 	new file:   assets/img-anuncios/1.jpeg 	new file:   assets/img-anuncios/2.jpeg 	new file:   assets/img-anuncios/3.jpeg 	new file:   assets/img-anuncios/4.jpeg 	new file:   assets/img-anuncios/5.jpeg 	new file:   assets/img-anuncios/6.jpeg 	new file:   assets/img-anuncios/7.jpeg 	new file:   assets/img-anuncios/8.jpeg 	new file:   assets/img-anuncios/9.jpeg 	modified:   css/anuncios.css 	modified:   css/nosotros.css 	modified:   index.html 	modified:   js/slider.js 	modified:   quienessomos.html

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,27 @@
-var swiper = new Swiper(".mySwiper", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+window.addEventListener('load' , function () { 
+  console.log('el contenido ha cargado');
+  
+  var imagenes =[]
+  
+  imagenes[0]='assets/slider3.jpg'
+  imagenes[1]='assets/slider2.jpg'
+  imagenes[2]='assets/slider1.jpg'
+  
+  var indiceImagenes=0;
+  
+  function cambiarImagenes(){
+  
+      document.PAGINACERTUSGROUP.src=imagenes[indiceImagenes];
+  
+  if (indiceImagenes < 2) {
+      indiceImagenes++; 
+      
+  }else{
+  indiceImagenes = 0;
+  }
+  
+  }
+  
+  setInterval(cambiarImagenes,1500);
+  
   });
