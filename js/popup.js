1e68615c0d3bea8466bbc2195badcf6e24a9b2e6
carloslorap <74977007@certus.edu.pe>
2022-12-09 22:38:07 -0500

    ERHERHERHGEWRG

--- js/popup.js
+++ js/popup.js
@@ -15,6 +15,20 @@ bntCerrarPopup.addEventListener('click',function(){
 
 
 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        600: {
+            slidesPerView: 3,
+        }
+    }
+});
+
 
 
 
